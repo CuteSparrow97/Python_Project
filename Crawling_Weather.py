9a018215d201e971f08c0fdec6d6b07931783255
Qute_Sparrow <dlckdgml345@naver.com>
2022-10-27 03:31:03 +0900

    시간대와 날씨 파싱 구현

--- Crawling_Weather.py
+++ Crawling_Weather.py
@@ -4,6 +4,7 @@ import requests
 import os
 import time
 import re
+from datetime import datetime
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
@@ -38,7 +39,75 @@ class Crawling_Weather():
         TextInput.submit()
         driver.implicitly_wait(1)
 
+        liHour = []         # 시각
+        liWeather = []      # 날씨
+        liTemperature = []  # 온도
+        liHumidity = []     # 습도
+        liWind = []         # 바람
+
+        # 현재 시각 가져오기
+        now = datetime.now()
+        liHour.append(now.hour)
+
+        # 현재 온도 가져오기
 
 
+        # 현재 시각 날씨 (현재 시각이 몇시인지 안나와있음)
+        strNowWeather = driver.find_element_by_class_name("weather_main").find_elements_by_class_name("blind")[0].text
+        print(strNowWeather)
+
+        # 현재 시각 습도
+        divInformation = driver.find_element_by_class_name("temperature_info")
+        dts = divInformation.find_elements_by_tag_name("dt")
+        dds = divInformation.find_elements_by_tag_name("dd")
+        strNowHumidity = ""
+        strNowWind = ""
+
+        nIndex = 0
+        for dt in dts:
+            ntitle = dt.text
+            if ("습도" in ntitle):
+                strNowHumidity = dds[nIndex].text
+            elif ("바람" in ntitle):
+                strNowWind = dds[nIndex].text
+                
+            nIndex +=1
+
+        print(strNowHumidity)
+        print(strNowWind)
+
+        # 시간대 별 날씨 파싱
+        divWeather_hourly = driver.find_element_by_xpath('//*[@id="main_pack"]/section[1]/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/div/div/div/div[2]')
+        ul = divWeather_hourly.find_element_by_tag_name("ul")
+        lis = ul.find_elements_by_tag_name("li")
+        
+        for li in lis:
+            strDay_Attr = li.get_attribute("data-day")
+            if strDay_Attr == "tomorrow":
+                break;
+
+            # 시간대 파싱
+            strHourly = li.text
+            re_1 = re.compile('^(\w{2}\D)')
+            word = re_1.search(strHourly)
+            strHour = word.group()
+            liHour.append(strHour)
+
+            # 날씨 파싱
+            re_1 = re.compile('\D\D(?=\\n)')
+            word = re_1.search(strHourly)
+            strWeather = word.group()
+            liWeather.append(strWeather)
+
+            b = 3
+
+        print(liHour)
+        print(liWeather)
+        a = 2
+        # 시간대 별 강수 파싱
+
+        # 시간대 별 바람 파싱
+
+        # 시간대 별 습도 파싱
 
         a = 1
